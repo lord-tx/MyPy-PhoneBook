b8ceeac3f3ce09d1af2b0b4d30ef19fca886fabf
lord-tx <thurggex@gmail.com>
2020-04-01 13:03:01 +0200

    Pushing the first batch

---
+++ MYPYPhoneBook.py
@@ -0,0 +1,351 @@
+import os
+import json
+
+# TODO: Initialize the JSON file with Default entry
+
+class Individual(object):
+    def __init__(self):
+        self.individual_array = {}
+
+    # One of the most important functions. It gives a window 
+    # for objects to request directly for the individual array
+    def get_array(self):
+        return self.individual_array
+
+    # This function might be useless
+    def get_phone_number(self):
+        return self.PhoneNumber
+
+    # This function eliminates the need for a complicated 
+    # constructor
+    def set_details(self, name, some_number):
+        self.FullName = name
+        self.PhoneNumber = some_number
+
+    # Returns true no matter what happens - Which is supposed to be changed
+    def save(self):
+        self.individual_array.update ({"name" : self.FullName, "number" : self.PhoneNumber})
+        return True
+
+
+class PhoneBook(object):
+
+    def __init__(self):
+        # This array would hold the initial array to be
+        # manipulated during creation of phone data and for 
+        # other array manipulations
+        self.Phone_Record = []
+
+        self.number_of_entries = 0
+
+        # I'm trying to initialize an array to
+        # take the individual object's array
+        self.ind_array = {}
+
+        # This Total Array would hold the 
+        # specific contents of the JSON file 
+        self.TotalArray = []
+
+        # get current directory
+        currentDirectory = os.getcwd()
+        # store file name in a variable
+        file_name = r'/phonebook.json'
+        # full path of json file
+        total_path = currentDirectory + file_name
+        # check if json file exists
+        if os.path.isfile(total_path) and os.access(total_path, os.R_OK):
+            # read json file
+            with open('phonebook.json', 'r') as json_file:
+                self.TotalArray = json.load(json_file)
+
+    # This function eliminates the need for a complicated constructor
+    # It takes a single parameter and sets the in_array var to its array
+    def set_person(self, person):
+        self.ind_array = person.get_array()
+    
+    # Allows the function to reset the link to the JSON file after 
+    # every operation in the main function
+    def startJSON_link(self):
+        # get current directory
+        currentDirectory = os.getcwd()
+        # store file name in a variable
+        file_name = r'/phonebook.json'
+        # full path of json file
+        total_path = currentDirectory + file_name
+        # check if json file exists
+        if os.path.isfile(total_path) and os.access(total_path, os.R_OK):
+            # read json file
+            with open('phonebook.json', 'r') as json_file:
+                self.TotalArray = json.load(json_file)
+    
+    def Add_Phone_Entry(self, ind_array):
+        # print (json_file)     -- Debugging purposes only
+        # print(self.TotalArray) 
+
+        # Array to hold individual array incase the variable exists
+        reset_array = ind_array
+
+        # Call the JSON file here and scan for
+        # existing details in it
+        if ind_array["number"] in ([sub["number"] for sub in self.TotalArray]):
+            print ("Duplicate Detail exists")
+            # Create an array variable that holds the dict to retrieve the name
+            array = self.Lookup_Number(ind_array["number"])
+            print ("Number exists with Name: {}".format(array["name"]))
+
+            # This is for the API capabilities
+            error = "Duplicate Detail exists"
+            error_details = "Number exists with Name: {}".format(array["name"])
+
+            # Return a dictionary containing the error and error details
+            return ({"error" : error, "details" : error_details})
+
+        elif ind_array["number"] not in ([sub["number"] for sub in self.TotalArray]):
+            self.Save_Phone_Entry(ind_array)
+            return "Successful"
+
+    # A simple function that opens the JSON and dumps the new JSON 
+    def Save_Phone_Entry(self, ind_array):
+        self.Phone_Record = ind_array # Line might not be needed 
+        self.TotalArray.append(self.Phone_Record)
+        # Call the JSON file here and pop
+        # details into it
+        with open('phonebook.json', 'w') as phonebook:
+            json.dump(self.TotalArray, phonebook, indent=4)
+    
+    # Function to allow for last minute change of heart
+    def confirm_change(self, type_of="delete"):
+        # Takes two parameters that allows the user of the function 
+        # to select between the type of action to perform - Default is 'delete'
+        if type_of == "delete":
+            print ("Do you really want to delete this? (Y)/N (Press AnyKey to Select Default)")
+            response = str(input())
+            if response != "N" or "n":
+                return True
+            else:
+                return False
+
+        # For the alter part, it sets two new variables that are visible 
+        # throughout the class and are utilized in the Update_Entry function
+        if type_of == "alter":
+            print ("Enter New Name: ")
+            self.new_name = str(input()).lower()
+            print ("Enter New Number: ")
+            self.new_number = int(input())
+            print ("\nDo you really want to alter this? (Y)/N (Press AnyKey to Select Default)")
+            response = str(input())
+            if response != "N" or "n":
+                return True
+            else:
+                return False
+
+    def Delete_Entry(self, name, number):
+        # print (self.TotalArray) -- Debugging purposes only
+        # Boolean to indicate if a value is found in the list or not
+        available = False
+        # Function sets record to run through the Total Array
+        for record in range(len(self.TotalArray)):
+            # if a matching record is found...
+            if ((self.TotalArray[record]["name"] == name) & (self.TotalArray[record]["number"] == number)):
+                # Verify if they want to delete it
+                
+                # For the API functionality, it implements it's 
+                # own confirm_change function.
+                del self.TotalArray[record]
+                with open('phonebook.json', 'w') as phonebook:
+                        json.dump(self.TotalArray, phonebook, indent=4)
+                    # print (self.TotalArray) -- Debugging purposes only
+                available = True
+
+                # For the standard CMD call
+                """
+                if (self.confirm_change("delete") == True):
+                    # Nuke em!
+                    del self.TotalArray[record]
+                    print("Entry Deleted Successfully! \n")
+
+                    # For the API call
+                    return "Entry Deleted Successfully! \n"
+
+                    # Save the deleted list back to the JSON
+                    with open('phonebook.json', 'w') as phonebook:
+                        json.dump(self.TotalArray, phonebook, indent=4)
+                    # print (self.TotalArray) -- Debugging purposes only
+                    available = True
+                else:
+                    pass
+                    # Adding anything here ruins this code
+                """
+                break
+                    # This stops the app from doing any more iterations
+                    # VERY IMPORTANT as it also reduces the app overhead
+                    
+
+            else:
+                pass
+                # Adding anything here ruins this code
+
+        if available == True:
+            return "Entry Deleted Successfully! \n"
+        else:
+            # print("\n\t Check the name or number! \n")
+            # For the API call
+            return ("\n\t ERROR DELETING! \n Check the name or number! \n")
+        # Call the JSON file here and pop
+        # details out of it
+
+    def Update_Entry(self, name, number):
+        # print (self.TotalArray) -- Debugging purposes only
+        # Boolean to indicate if a value is found in the list or not
+        available = False
+        for record in self.TotalArray:
+            # Traverse the array linearly (This should be binary in future)
+            if ((record["name"] == name) & (record["number"] == number)):
+                # Call the confirm_change function incase of any last 
+                # minute change of heart
+                if (self.confirm_change("alter") == True):
+                    record["name"] = self.new_name
+                    record["number"] = self.new_number
+                    # print (self.TotalArray) -- Debugging purpose only
+
+                    # Save the updated list back to the JSON
+                    with open('phonebook.json', 'w') as phonebook:
+                        json.dump(self.TotalArray, phonebook, indent=4)
+                    # Set boolean to True to enable other manipulation
+                    available = True
+                else:
+                    pass
+                    # Adding anything here ruins this code
+                break
+                    # This stops the app from doing any more iterations
+
+        if available == True:
+            print ("Successful!")
+        else:
+            print("\nNumber or name not found!\n")
+
+
+    def Lookup_Number(self, number):
+        # Initialize available variable
+        available = False
+        # Traverse the array linearly (This should be binary in future)
+        for record in self.TotalArray:
+            if (record["number"] == number):
+                # Return an dict that's subscript-able
+                # Nice Eh? 
+                return ({"name" : record["name"], "number" : record["number"]})
+
+                # Set boolean to True to enable other manipulation
+                available = True
+
+            else:
+                pass
+                # Adding anything here ruins this code
+                
+        if available == True:
+            pass
+        else:
+            print("\nNumber doesn't exist!")
+
+
+        
+
+# Display function takes no parameters and returns
+# response based on user_selection - For Simplification
+def Display():
+    print("###########################")
+    print("###   MYPY PHONE BOOK   ###")
+    print("###########################")
+    print("1: Add New Entry")
+    print("2: Delete Entry")
+    print("3: Update Entry")
+    print("4: Lookup Number")
+    print("5: QUIT")
+    try:
+        response = int(input(">: "))
+        return response
+    except (ValueError):
+        print ("\n\tEnter one of the numbers!\n")
+    
+# Simple function that grabs user input and makes it a string
+def GetName():
+    print("FullName: ")
+    name = str(input()).lower()
+    return name
+
+# Simple function that grabs user input and makes it an integer
+def GetNumber():
+    print("Number: ")
+    try:
+        number = int(input())
+    except ValueError:
+        print("Enter Only Numbers!")
+        return GetNumber()
+
+    return number
+
+# Main Driver Function
+def MyPyFunction():
+    # Initialization of objects of both classes 
+    # with the default (empty) constructors
+    individual = Individual()
+    phone_book = PhoneBook()
+
+    exit_signal = False
+    while (exit_signal == False):
+        display_response = Display()
+        # This startJSON_link function resets the Program's 
+        # own array that holds the content of the JSON file -> self.TotalArray[]
+        phone_book.startJSON_link()
+
+        if display_response == 1:
+            name = GetName()
+            number = GetNumber()
+            # The set_detail function is only used during 
+            # the add_entry selection
+            individual.set_details(name, number)
+            # Variable to hold the boolean value of the individual method 
+            # save()
+            try_save = individual.save()
+            if (try_save == True):
+                # When the operation is complete - the get_array 
+                # method has been populated by the save function
+
+                # Set the phone book to point to an individual object
+                phone_book.set_person(individual)
+                phone_book.Add_Phone_Entry(individual.get_array())
+            else:
+                print("\nCouldn't Save!\n")
+
+        # Other responses utilize the GetName() and GetNumber() 
+        # functions to simplify integer checking and avoid too many 
+        # try-catch statements
+
+        elif display_response == 2:
+            name = GetName()
+            number = GetNumber()
+            phone_book.Delete_Entry(name, number)
+
+        elif display_response == 3:
+            name = GetName()
+            number = GetNumber()
+            phone_book.Update_Entry(name, number)
+
+        elif display_response == 4:
+            number = GetNumber()
+            # Create array to retrieve dict from lookup
+            array = phone_book.Lookup_Number(number)
+            # Check if array is empty
+            if (array != None):
+                name = array["name"]
+                number = array["number"]
+                print("\n\n#################################")
+                print("|Name|: \t{}".format(name))
+                print("|Phone Number|: {}".format(number))
+                print("#################################\n")
+
+        elif display_response == 5:
+            exit_signal = True
+
+# Comment out this part of the code to let this work like a package
+# MyPyFunction()
---
+++ MyPy PhoneBook GUI.py
@@ -0,0 +1,359 @@
+import tkinter as tk
+import MYPYPhoneBook as PhoneBook
+from tkinter import messagebox
+
+# This first set of commented-out code is a simplistic GUI 
+# It just implements the LookUp function in a minimalistic
+# manner
+
+"""
+win = tk.Tk()
+win.title("Lookup Number")
+
+search_entry_number = tk.IntVar()
+search_entry = tk.Entry(win, width=20, textvariable=search_entry_number)
+search_entry.grid(column=0, row=0, padx=10, pady=10)
+
+def LookupContact(phone_number):
+    print(phone_number)
+    phonebook = PhoneBook.PhoneBook()
+    details = phonebook.Lookup_Number(phone_number)
+    name = details["name"]
+    print (name)
+
+search_button = tk.Button(win, text="Search", command=lambda: LookupContact(search_entry_number.get()))
+search_button.grid(column=0, row=1, padx=10, pady=10)
+
+result_sheet = tk.Label(win, width=10, height=5)
+result_sheet.grid(column=0, row=3, padx=10, pady=10)
+result_sheet.grid_forget()
+
+win.mainloop()
+"""
+
+# This second set of commented out code is a BUILD-DESTROY kind
+# of GUI where one window calls out another window but it becomes
+# cluttered quickly
+
+# It also fails to implement the LookUp function (I'm still trying
+# to figure out why?)
+"""
+class GUILookUp(object):
+    def __init__(self):
+        self.win = tk.Tk()
+        self.win.title("LookUp Number")
+        self.text = tk.StringVar()
+
+        self.Look_up_frame = tk.Frame(self.win)
+        self.Look_up_frame.grid()
+
+        self.search_entry_number = tk.IntVar()
+        self.search_entry = tk.Entry(self.Look_up_frame, width=20, textvariable=self.search_entry_number)
+        self.search_entry.grid(column=0, row=0, padx=10, pady=10)
+        self.search_entry.focus()
+
+        self.search_button = tk.Button(self.Look_up_frame, text="Search", command= self.LookupContact)
+        self.search_button.grid(column=0, row=1, padx=10, pady=10)
+
+        self.result_sheet = tk.Label(self.Look_up_frame, textvariable=self.text)
+        self.result_sheet.grid(column=0, row=3, padx=10, pady=10)
+        self.result_sheet.grid_remove()
+
+    def LookupContact(self):
+        phone_number = self.search_entry_number.get()
+        print(phone_number)
+        phonebook = PhoneBook.PhoneBook()
+        details = phonebook.Lookup_Number(phone_number)
+        name = str(details["name"])
+        # self.result_sheet.grid()
+        # self.text.set("Name: " + name + "\n Phone Number: " + (str(details["number"])))
+
+# lookup = GUILookUp()
+# lookup.win.mainloop()
+
+class GUIMENU(object):
+    def __init__(self):
+        self.gui_win = tk.Tk()
+        self.gui_win.title("GUI MyPy PhoneBook")
+
+        welcome = "\n\n--   MYPY PHONE BOOK   --\n\n"
+
+        self.Welcome_Label = tk.Label(self.gui_win, text=welcome)
+        self.Welcome_Label.grid()
+
+        self.Add_Button = tk.Button(self.gui_win, text="Add New Entry")
+        self.Add_Button.grid()
+
+        self.Delete_Button = tk.Button(self.gui_win, text="Delete Entry")
+        self.Delete_Button.grid()
+
+        self.Update_Button = tk.Button(self.gui_win, text="Update Entry")
+        self.Update_Button.grid()
+
+        self.Lookup_Button = tk.Button(self.gui_win, text="Lookup Number", command=self.Lookup_Button)
+        self.Lookup_Button.grid()
+
+        self.Exit_Button = tk.Button(self.gui_win, text="Exit", command=quit)
+        self.Exit_Button.grid()
+
+    def Lookup_Button(self):
+        lookup = GUILookUp()
+        lookup.win.mainloop()
+
+guimenu = GUIMENU()
+guimenu.gui_win.mainloop()
+
+"""
+
+# This third implementation directly accesses the Tkinter class by inheriting
+# directly from it and implementing other classes as frames
+
+# It works perfectly. Except for the unimplemented parts.
+
+class SampleApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("MYPY Phone Book")
+
+        # self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (MenuPage, GUILookUp, GUIAddEntry, GUIDeleteEntry):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("MenuPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class MenuPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        welcome = "\n\n--   MYPY PHONE BOOK   --\n\n"
+
+        # These parts are kinda explanatory - We just create a bunch of labels
+        # and buttons
+
+        self.Welcome_Label = tk.Label(self, text=welcome)
+        self.Welcome_Label.grid(column=2, columnspan=3, padx=10, pady=10)
+
+        self.Add_Button = tk.Button(self, text="Add New Entry", command=lambda: controller.show_frame("GUIAddEntry"))
+        self.Add_Button.grid(column=2, columnspan=3, padx=10, pady=10)
+
+        self.Delete_Button = tk.Button(self, text="Delete Entry", command=lambda: controller.show_frame("GUIDeleteEntry"))
+        self.Delete_Button.grid(column=2, columnspan=3, padx=10, pady=10)
+
+        self.Update_Button = tk.Button(self, text="Update Entry")
+        self.Update_Button.grid(column=2, columnspan=3, padx=10, pady=10)
+
+        self.Lookup_Button = tk.Button(self, text="Lookup Number", command=lambda: controller.show_frame("GUILookUp"))
+        self.Lookup_Button.grid(column=2, columnspan=3, padx=10, pady=10)
+
+        self.Exit_Button = tk.Button(self, text="Exit", command=quit)
+        self.Exit_Button.grid(column=2, columnspan=3, padx=10, pady=10)
+
+    
+    def Back(self):
+        self.result_sheet.grid_remove()
+        self.controller.show_frame("MenuPage")
+
+
+class GUIAddEntry(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        button = tk.Button(self, text="Back", command=self.Back)
+        button.grid(column=0, row=0, padx=10, pady=10)
+
+        name_label = tk.Label(self, text="Name: ")
+        name_label.grid(column=0, row=1, padx=10, pady=10)
+        number_label = tk.Label(self, text="Number: ")
+        number_label.grid(column=0, row=2, padx=10, pady=10)
+
+        self.search_entry_name = tk.StringVar()
+        self.search_entry_na = tk.Entry(self, width=20, textvariable=self.search_entry_name)
+        self.search_entry_na.grid(column=1, row=1, padx=10, pady=10)
+        self.search_entry_na.focus()
+
+        self.search_entry_number = tk.IntVar()
+        self.search_entry_no = tk.Entry(self, width=20, textvariable=self.search_entry_number)
+        self.search_entry_no.grid(column=1, row=2, padx=10, pady=10)
+        self.search_entry_number.set("")
+
+        self.add_button = tk.Button(self, text="Add Contact", command=self.Add_Contact)
+        self.add_button.grid(column=0, row=3, padx=10, pady=10, columnspan=2)
+
+        self.text = tk.StringVar()
+        self.result_sheet = tk.Label(self, textvariable=self.text)
+        self.result_sheet.grid(column=0, row=4, padx=10, pady=10, columnspan=3)
+        self.result_sheet.grid_remove()
+
+    def Add_Contact(self):
+        name = self.search_entry_name.get()
+        phone_number = self.search_entry_number.get()
+        # Save the details in a dictionary, the add contact function
+        # takes a dictionary of details
+        ind_array = {"name" : name, "number" : phone_number}
+
+        # Create an instance (object) of the phonebook class
+        phonebook = PhoneBook.PhoneBook()
+        # Start the JSON link as usual
+        phonebook.startJSON_link()
+        result = phonebook.Add_Phone_Entry(ind_array)
+        self.result_sheet.grid()
+        if (result == "Successful"):
+            self.text.set(result)
+            self.search_entry_name.set("")
+            self.search_entry_number.set("")
+        else:
+            total_result = result["error"] + "\n" +result["details"]
+            self.text.set(total_result)
+            self.search_entry_name.set("")
+            self.search_entry_number.set("")
+
+    def Back(self):
+        self.result_sheet.grid_remove()
+        self.controller.show_frame("MenuPage")
+
+
+class GUIDeleteEntry(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        button = tk.Button(self, text="Back", command=self.Back)
+        button.grid(column=0, row=0, padx=10, pady=10)
+
+        name_label = tk.Label(self, text="Name: ")
+        name_label.grid(column=0, row=1, padx=10, pady=10)
+        number_label = tk.Label(self, text="Number: ")
+        number_label.grid(column=0, row=2, padx=10, pady=10)
+
+        self.search_entry_name = tk.StringVar()
+        self.search_entry_na = tk.Entry(self, width=20, textvariable=self.search_entry_name)
+        self.search_entry_na.grid(column=1, row=1, padx=10, pady=10)
+        self.search_entry_na.focus()
+
+        self.search_entry_number = tk.IntVar()
+        self.search_entry_no = tk.Entry(self, width=20, textvariable=self.search_entry_number)
+        self.search_entry_no.grid(column=1, row=2, padx=10, pady=10)
+        self.search_entry_no.focus()
+
+        self.delete_button = tk.Button(self, text="Delete Contact", command=self.Delete_Contact)
+        self.delete_button.grid(column=0, row=3, padx=10, pady=10, columnspan=2)
+
+        self.text = tk.StringVar()
+        self.result_sheet = tk.Label(self, textvariable=self.text)
+        self.result_sheet.grid(column=0, row=4, padx=10, pady=10, columnspan=3)
+        self.result_sheet.grid_remove()
+    
+
+    def Delete_Contact(self):
+        try:
+            name = self.search_entry_name.get()
+            phone_number = self.search_entry_number.get()
+        except (_tkinter.TclError):
+            
+        # Create an instance (object) of the phonebook class
+        phonebook = PhoneBook.PhoneBook()
+        # Start the JSON link as usual
+        phonebook.startJSON_link()
+        answer = self.Confirm_Change()
+        self.result_sheet.grid()
+        # Get a result from the Delete_Entry Function
+        result = phonebook.Delete_Entry(name, phone_number)
+
+        if (answer == True):
+            self.text.set(result)
+            self.search_entry_name.set("")
+            self.search_entry_number.set("")
+        else:
+            
+            self.text.set(total_result)
+            self.search_entry_name.set("")
+            self.search_entry_number.set("")
+
+    def Confirm_Change(self):
+        message_box = messagebox.askquestion("Proceed", "Do you really want to delete this contact?")
+        if message_box == 'yes':
+            return True
+        else:
+            return False
+
+    def Back(self):
+        self.result_sheet.grid_remove()
+        self.controller.show_frame("MenuPage")
+
+
+class GUILookUp(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        button = tk.Button(self, text="Back", command=self.Back)
+        button.grid(column=0, row=0, padx=10, pady=10)
+
+        self.search_entry_number = tk.IntVar()
+        self.search_entry = tk.Entry(self, width=20, textvariable=self.search_entry_number)
+        self.search_entry.grid(column=0, row=1, padx=10, pady=10)
+        self.search_entry.focus()
+
+        self.search_button = tk.Button(self, text="Search", command= self.LookupContact)
+        self.search_button.grid(column=0, row=2, padx=10, pady=10)
+
+        self.text = tk.StringVar()
+        self.result_sheet = tk.Label(self, textvariable=self.text)
+        self.result_sheet.grid(column=0, row=4, padx=10, pady=10)
+        self.result_sheet.grid_remove()
+
+    def LookupContact(self):
+        phone_number = self.search_entry_number.get()
+        print(phone_number)
+        phonebook = PhoneBook.PhoneBook()
+        details = phonebook.Lookup_Number(phone_number)
+        name = str(details["name"])
+        self.result_sheet.grid()
+        self.text.set("Name: " + name + "\n Phone Number: " + (str(details["number"])))
+
+    def Back(self):
+        self.result_sheet.grid_remove()
+        self.controller.show_frame("MenuPage")
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 2") #, font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
